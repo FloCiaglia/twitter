bae28ba03e0b1aba6ef7b4ae89f0648ffb14ce8c
FloCiaglia <floriana.ciaglia@gmail.com>
2020-10-05 20:41:18 -0600

    Stay logged in

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -19,11 +19,22 @@ class LoginViewController: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    
     @IBAction func onLoginButton(_ sender: Any) {
         
         let url = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: url, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: {(Error) in print("Could not log in")})
     }
