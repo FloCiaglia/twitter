81da4912da0fd25582aa0e3ea0de2c9e67a18606
FloCiaglia <floriana.ciaglia@gmail.com>
2020-10-15 20:06:42 -0600

    Implemented favoriting a tweet and retweeting a tweet

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -20,6 +20,8 @@ class HomeTableTableViewController: UITableViewController {
         //loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 200
     
     }
     override func viewDidAppear(_ animated: Bool) {
@@ -109,6 +111,12 @@ class HomeTableTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        
         
         return cell
     }
